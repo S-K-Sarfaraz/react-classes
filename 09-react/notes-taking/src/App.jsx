066791b720b6cdba4c5d7b62ff2729c90ec78b08
S K Sarfaraz <sarfaraz7856sk@gmail.com>
2026-03-12 13:03:39 +0530

    note delete functionality

--- 09-react/notes-taking/src/App.jsx
+++ 09-react/notes-taking/src/App.jsx
@@ -1,23 +1,32 @@
 import React, { useState } from "react";
 
 const App = () => {
-    const [title, setTitle] = useState('')
-    const [desc, setDesc] = useState('')
+    const [title, setTitle] = useState("");
+    const [desc, setDesc] = useState("");
 
-    const [task, setTask] = useState([])
+    const [task, setTask] = useState([]);
 
     const submitNote = (e) => {
         e.preventDefault();
 
-        const copyTask = [...task] 
+        const copyTask = [...task];
 
-        copyTask.push({title, desc})
+        copyTask.push({ title, desc });
 
-        setTask(copyTask)
-        console.log(task)
+        setTask(copyTask);
+        console.log(task);
 
-        setTitle('')
-        setDesc('')
+        setTitle("");
+        setDesc("");
+    };
+
+    const deleteNote = (index) => {
+        console.log(index)
+        const copyNote = [...task]; // [1,2,3,5,6] arr.splice(3,1)
+
+        copyNote.splice(index, 1);
+
+        setTask(copyNote);
     };
     return (
         <div className="m-5">
@@ -30,8 +39,8 @@ const App = () => {
                         placeholder="Enter the title..."
                         className="h-3 border p-3 rounded hover:bg-gray-100"
                         value={title}
-                        onChange={e=>{
-                          setTitle(e.target.value)
+                        onChange={(e) => {
+                            setTitle(e.target.value);
                         }}
                     />
 
@@ -41,8 +50,8 @@ const App = () => {
                         placeholder="Enter the Description..."
                         className="border p-3 rounded h-30 hover:bg-gray-100"
                         value={desc}
-                        onChange={e=>{
-                          setDesc(e.target.value)
+                        onChange={(e) => {
+                            setDesc(e.target.value);
                         }}
                     />
 
@@ -60,14 +69,32 @@ const App = () => {
 
             <div>
                 <div className="text-2xl font-bold mb-3 mt-7">All Notes</div>
-                {task.map((e, index)=>{
-                  return (
-                    <div key={index} className="bg-gray-100 h-60 w-50 border rounded-2xl">
-                      <div>{e.title}</div>
-                      <div>{e.desc}</div>
-                    </div>
-                  )
-                })}
+                <div className="flex gap-4 flex-wrap">
+                    {task.map((e, index) => {
+                        return (
+                            <div
+                                key={index}
+                                className="bg-gray-100 h-60 w-50 border p-4 rounded-2xl"
+                            >
+                                <div className="text-2xl font-semibold">
+                                    {e.title}
+                                </div>
+                                <div>
+                                    {e.desc}
+                                    {index}
+                                </div>
+                                <div className="flex justify-center relative">
+                                    <button
+                                        onClick={() => deleteNote(index)}
+                                        className="absolute top-30 px-4 py-1 bg-red-400 rounded-2xl text-white"
+                                    >
+                                        Delete
+                                    </button>
+                                </div>
+                            </div>
+                        );
+                    })}
+                </div>
             </div>
         </div>
     );
@@ -75,5 +102,5 @@ const App = () => {
 
 export default App;
 
-//  Note jo ban raha hai us per ek button lagana hai delete ka jab us per click karun to wo wala note delete ho jaye 
-//  splice array 
+//  Note jo ban raha hai us per ek button lagana hai delete ka jab us per click karun to wo wala note delete ho jaye
+//  splice array
