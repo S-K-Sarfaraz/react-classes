6a87fc47cf546cf9eba3e3bf13d63587b4f2704c
S K Sarfaraz <sarfaraz7856sk@gmail.com>
2026-03-23 13:17:35 +0530

    simple react router

---
+++ 12-react/simple-router/src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <div>
+        <div className='h-15 text-gray-800 font-bold p-5 flex justify-between bg-amber-200'>
+        <div>Raect-Router</div>
+        <div className='flex gap-5'>
+            <Link to='/'>Home</Link>
+            <Link to='/about'>About</Link>
+            <Link to='/contact'>Contact</Link>
+          {/* <a href="/">Home</a>
+          <a href="/about">About</a>
+          <a href="/contact">Contact</a> */}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
