0a5030042f8faa07060a46d2ff3f70d1a8c6e6b1
S K Sarfaraz <sarfaraz7856sk@gmail.com>
2026-05-12 12:28:20 +0530

    nextjs seo metadata

--- 01-next/first-project/src/app/about/page.jsx
+++ 01-next/first-project/src/app/about/page.jsx
@@ -1,5 +1,11 @@
 import React from 'react'
 
+
+export const metadata = {
+  title: 'About page',
+  description: 'This is the about page'
+}
+
 const About = () => {
   return (
     <div className='flex justify-center items-center h-screen text-8xl font-bold'>About</div>
--- 01-next/first-project/src/app/contacts/page.jsx
+++ 01-next/first-project/src/app/contacts/page.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 
+export const metadata ={
+  title: 'Contact us',
+  description: 'this is the contact page'
+}
+
 const Contact = () => {
   return (
     <div className='flex justify-center items-center h-screen text-8xl font-bold'>Contact</div>
